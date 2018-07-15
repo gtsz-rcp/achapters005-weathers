2e3fda8ec81324da1b3694f4ee549653b3c517eb
seunghaekim <aka.seunghaekim@gmail.com>
2018-07-15 19:38:41 +0900

    initialize

--- main.py
+++
@@ -1,6 +0,0 @@
-import BartelbyMachine.main as bartelby
-# from bartelby import Bartelby
-
-bartleby = bartelby.Bartelby()
-bartleby.addTableOfContent('toc.lump-of-meat.json');
-bartleby.markdowntolatex()
